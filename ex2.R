49e02c786e20f9b7fdde78393c424597294a332b
antoniovard@gmail.com <HSnqskvg3bST6b>
2022-04-06 17:12:00 +0100

    ex2: Configurar o dataframe, tentar usar o subset

--- ex2.R
+++ ex2.R
@@ -1,9 +1,10 @@
 library(readxl)
 library(ggplot2)
 
-xlsx_ex2 = "excels/EsperancaVida.xlsx"
-UE_excel = read_xlsx(xlsx_ex2, sheet = "Quadro",)
 
-UE_data = data.frame(subset(UE_excel,c(5,8,11)))
+xlsx_ex2 = "EsperancaVida.xlsx"
+UE_excel = read_excel(xlsx_ex2, sheet = 1,skip=8,n_max=60)
+
+UE_data = data.frame(subset(UE_excel,),row.names = 1)
 
 p = ggplot(data = UE_data) + geom_histogram(mapping = aes("Tempo"))
