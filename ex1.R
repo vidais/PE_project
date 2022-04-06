49421077a62c980c6c2f298b3b5c9425c7f42242
antoniovard@gmail.com <HSnqskvg3bST6b>
2022-04-06 16:38:46 +0100

    ex1: Falta separar y do fill em 2 barras, como fazer isso?

--- ex1.R
+++ ex1.R
@@ -8,10 +8,13 @@ library(dplyr)
 #Excel
 xlsx_ex1<-"ResiduosperCapita.xlsx"
 UE_excel<-read_excel(xlsx_ex1,sheet = 1,skip=11,n_max = 31,col_types = c("text","numeric","numeric"))
-names(UE_excel)<-c("Country","2014","2018")
+names(UE_excel)<-c("country","X2004","X2018")
 
 #Descobrir as linhas a serem usadas
-rows<-c(grep("LV",UE_excel$Anos),grep("UK",UE_excel$Anos),grep("SK",UE_exciel$Anos))
-UE_data<-data.frame(UE_excel[rows,],row.names =1)
+rows<-c(grep("LV",UE_excel$country),grep("UK",UE_excel$country),grep("SK",UE_excel$country))
+UE_data<-as.data.frame(UE_excel[rows,])
 
-g<-ggplot(UE_data,aes(x=1))+geom_bar()
+g<-ggplot(data=UE_data,aes(x=country,y=X2004,fill=X2018))
+g<-g+geom_bar(stat = "identity",width = 0.5,position = position_dodge(),color="black")
+g<-g+theme_minimal()
+g
