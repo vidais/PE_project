993da608eda1048c62092422eea8cf634dcb83b2
antoniovard@gmail.com <HSnqskvg3bST6b>
2022-04-06 15:46:38 +0100

    ex1:Nomes das colunas são atualizados corretamente

--- ex1.R
+++ ex1.R
@@ -6,11 +6,12 @@ library(ggplot2)
 library(dplyr)
 
 #Excel
-xlsx_ex1<-"excels/ResiduosperCapita.xlsx"
+xlsx_ex1<-"ResiduosperCapita.xlsx"
 UE_excel<-read_excel(xlsx_ex1,sheet = 1,skip=11,n_max = 31,col_types = c("text","numeric","numeric"))
+names(UE_excel)<-c("Country","2014","2018")
 
 #Descobrir as linhas a serem usadas
-rows<-c(grep("LV",UE_excel$Anos),grep("UK",UE_excel$Anos),grep("SK",UE_excel$Anos))
+rows<-c(grep("LV",UE_excel$Anos),grep("UK",UE_excel$Anos),grep("SK",UE_exciel$Anos))
 UE_data<-data.frame(UE_excel[rows,],row.names =1)
 
-g<-ggplot(UE_data,aes("País"))+geom_bar()
+g<-ggplot(UE_data,aes(x=1))+geom_bar()
