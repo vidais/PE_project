4e68e5d22ce5e152e37de3078ada20c5cb8004bf
antoniovard@gmail.com <HSnqskvg3bST6b>
2022-05-10 14:48:53 +0100

    Ex1: falta mudar o fill para aparecerem 2 barras

--- ex1.R
+++ ex1.R
@@ -12,11 +12,11 @@ names(UE_excel)<-c("country","X2004","X2018")
 
 #Descobrir as linhas a serem usadas
 rows<-c(grep("LV",UE_excel$country),grep("UK",UE_excel$country),grep("SK",UE_excel$country))
-UE_data<-as.data.frame(UE_excel[rows,])
+UE_data<-data.frame(UE_excel[rows,])
 
-g<-ggplot(data=UE_data,aes(x=country))
-g<-g+geom_histogram(aes(y=X2004),stat="identity",width = 0.5,color="green",position = position_dodge(width = 0.5),fill="blue")
-g<-g+geom_histogram(aes(y=X2018),stat="identity",width = 0.5,position = "dodge",fill="green")
-g<-g+theme_minimal()+labs(title = "Residuos Per Capita em diferentes Paises da UE",x="Pais",X2004="2004",X2018="2018")
+g<-ggplot(data=UE_data,aes(x=country,y=X2004,fill=X2018))
+g<-g+geom_bar(stat="identity",position = "dodge",width = 0.7)
+#g<-g+geom_histogram(aes(y=X2018),stat="identity",width = 0.5,fill="green")
+g<-g+labs(title = "Residuos Per Capita em diferentes Paises da UE",x="Pais",X2004="2004",X2018="2018")
 g
 
--- ex2.R
+++ ex2.R
@@ -3,10 +3,9 @@ library(ggplot2)
 
 
 xlsx_ex2 <- "EsperancaVida.xlsx"
-UE_excel <- read_excel(xlsx_ex2, sheet = 1,skip=8,n_max=60)
+UE_excel <- read_excel(xlsx_ex2, sheet = 1,skip=8,n_max=(2019-1960),row.names(1))
 
-UE_data <- data.frame(subset(UE_excel,),row.names = 1)
-
-#col_nr<-c(grep("LV",UE_excel$country),grep("UK",UE_excel$country),grep("SK",UE_excel$country))
+UE_data <- data.frame(subset(UE_excel,))
 
 p <- ggplot(data = UE_data,aes(x=)) + geom_histogram()
+p
