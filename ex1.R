c187eb927bd88e153ffbf1b597b8cee4f115ca57
antoniovard@gmail.com <HSnqskvg3bST6b>
2022-05-10 16:04:31 +0100

    Ex1:Completo mas podia melhorar legendas

--- ex1.R
+++ ex1.R
@@ -4,19 +4,20 @@
 library(readxl)
 library(ggplot2)
 library(dplyr)
+library(reshape2)
 
 #Excel
 xlsx_ex1<-"ResiduosperCapita.xlsx"
 UE_excel<-read_excel(xlsx_ex1,sheet = 1,skip=11,n_max = 31,col_types = c("text","numeric","numeric"))
-names(UE_excel)<-c("country","X2004","X2018")
+names(UE_excel)<-c("country","2004","2018")
 
 #Descobrir as linhas a serem usadas
 rows<-c(grep("LV",UE_excel$country),grep("UK",UE_excel$country),grep("SK",UE_excel$country))
-UE_data<-data.frame(UE_excel[rows,])
+UE_data<-melt(data.frame(UE_excel[rows,]),id.vars="country")
 
-g<-ggplot(data=UE_data,aes(x=country,y=X2004,fill=X2018))
+g<-ggplot(data=UE_data,aes(x=country,y=value,fill=variable))
 g<-g+geom_bar(stat="identity",position = "dodge",width = 0.7)
 #g<-g+geom_histogram(aes(y=X2018),stat="identity",width = 0.5,fill="green")
-g<-g+labs(title = "Residuos Per Capita em diferentes Paises da UE",x="Pais",X2004="2004",X2018="2018")
+g<-g+labs(title = "Residuos Per Capita em diferentes Paises da UE",x="Pais",variable="2004")
 g
 
--- ex2.R
+++ ex2.R
@@ -3,8 +3,8 @@ library(ggplot2)
 
 
 xlsx_ex2 <- "EsperancaVida.xlsx"
-UE_excel <- read_excel(xlsx_ex2, sheet = 1,skip=8,n_max=(2019-1960),row.names(1))
-
+UE_excel <- read_excel(xlsx_ex2, sheet = 1,skip=8,n_max=(2019-1960))
+rows<-c(grep("DE",UE_excel[0,]),grep("BG",UE_excel[0,]),grep("DK",UE_excel[0,]))
 UE_data <- data.frame(subset(UE_excel,))
 
 p <- ggplot(data = UE_data,aes(x=)) + geom_histogram()
