5d0e4374ff66458b7a226ea2a37f30dd4d34a6cd
vidais <antonio.vidais@tecnico.ulisboa.pt>
2022-06-02 21:06:39 +0100

    Finished ex3 Gonna start ex5 & ex7 from Rita

--- ex2.R
+++ ex2.R
@@ -7,9 +7,9 @@ ex2_excel <- read_excel("EsperancaVida.xlsx", sheet = 1,skip=8,n_max=(2020-1960+
 country<-c("Alemanha", "Bulgaria", "Dinamarca")
 cols<-c(grep("DE",names(ex2_excel)),grep("BG",names(ex2_excel)),grep("DK",names(ex2_excel)))
 
-ex2_data <- data.frame(ex2_excel[cols])
+ex2_data <- data.frame(ex2_excel[43:60,cols])
 ex2_data[ex2_data == 0] <- NA
-time<-1960:2020
+time<-2002:2019
 
 p <- ggplot(data = ex2_data,aes(x=time))
 p <- p+geom_line(aes(y=ex2_data[,1],color="Alemanha"))+geom_line(aes(y=ex2_data[,4],color="Bulgaria"))+ geom_line(aes(y=ex2_data[,7],color="Dinamarca"))
--- ex3.R
+++ ex3.R
@@ -2,8 +2,12 @@ library(readxl)
 library(ggplot2)
 library(reshape2)
 
-ex3_excel <- read_excel("QualidadeARO3.xlsx", sheet = 1)
+ex3_excel <- read_excel("QualidadeARO3.xlsx", sheet = 1,col_types = "numeric")
 cols<-c(grep("Paio-Pires",names(ex3_excel)),grep("Ihavo",names(ex3_excel)))
 ex3_data <- melt(data.frame(ex3_excel[cols]),id.vars=0)
 
-g<-ggplot(ex3_data,aes(x=0:)+geom_histogram(binwidth = 10)
+time=1:17568
+
+g<-ggplot(data=ex3_data,aes(x=value,fill=variable))+geom_histogram(binwidth=7)
+g<-g+labs(title = "Niveis de Ozono em 2 estacoes")
+g
